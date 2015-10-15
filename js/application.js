1bde4128fe0d2797bff6f8f9a9920b86c9bbbfb7
Kushagra Singh <singh.kushagra.1996@gmail.com>
2015-10-15 09:58:47 +0530

    Changed main application function

--- js/application.js
+++ js/application.js
@@ -1,4 +1,23 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+// 0: up, 1: right, 2: down, 3: left
+
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+
+	function AI() {
+	    setTimeout(function () {
+
+	    	var future = 3;
+
+	    	if (game.grid.availableCells().length <= 5)
+	    		future = 4;
+
+			game.move(game.getBestMove(future));
+
+			AI();
+
+	    }, 200);
+	}
+
+	AI();
 });
